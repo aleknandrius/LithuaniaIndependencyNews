53ce92eed05dbf2ea705b0b83ab4d40a592e005c
aleknandrius <aleknandrius@gmail.com>
2018-03-08 12:15:06 +0200

    Add Splash screen

--- app/src/main/java/com/telesoftas/lithuaniaindependencynews/MainActivity.kt
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.telesoftas.lithuaniaindependencynews
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    companion object {
+        fun createIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 }
---
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/landing/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.telesoftas.lithuaniaindependencynews.landing
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.telesoftas.lithuaniaindependencynews.MainActivity
+
+class SplashScreenActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(MainActivity.createIntent(this))
+        finish()
+    }
+}
