c967ef55a58f6515e7af3ab5400ad6ef6a93253c
aleknandrius <aleknandrius@gmail.com>
2018-03-09 15:47:00 +0200

    Add date formater

---
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/utils/formater/DateFormater.kt
@@ -0,0 +1,5 @@
+package com.telesoftas.lithuaniaindependencynews.utils.formater
+
+interface DateFormater {
+    fun getFormatedString(date: String): String
+}
---
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/utils/formater/DateFormaterImpl.kt
@@ -0,0 +1,24 @@
+package com.telesoftas.lithuaniaindependencynews.utils.formater
+
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateFormaterImpl: DateFormater {
+    override fun getFormatedString(date: String): String {
+        return try {
+            var parsedDate = SimpleDateFormat(DATE_FORMAT_PARSED).parse(date)
+            val dateFormatToShow = SimpleDateFormat(DATE_FORMAT_TO_SHOW, Locale.ENGLISH)
+            dateFormatToShow.format(parsedDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            date
+        }
+    }
+
+    companion object {
+        const val DATE_FORMAT_PARSED = "yyyy-mm-dd'T'hh:mm:ss"
+        const val DATE_FORMAT_TO_SHOW = "yyyy-MMMM-dd hh:mm"
+    }
+
+}
