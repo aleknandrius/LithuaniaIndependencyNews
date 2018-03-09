2583ddefd517017f4b964063ebe9d6dc7135a0ee
aleknandrius <aleknandrius@gmail.com>
2018-03-09 15:50:12 +0200

    Add LeakCanary dependency

--- app/src/main/java/com/telesoftas/lithuaniaindependencynews/LithuaniaIndependencyNewsApplication.kt
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/LithuaniaIndependencyNewsApplication.kt
@@ -1,8 +1,18 @@
 package com.telesoftas.lithuaniaindependencynews
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+
 
 class LithuaniaIndependencyNewsApplication : Application() {
     val dependencyRetriever: DependencyRetriever = DependencyRetriever(this)
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
+    }
 }
 
