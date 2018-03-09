5bc27d6ad45b839e9495da57269856db759fffc2
aleknandrius <aleknandrius@gmail.com>
2018-03-09 10:52:18 +0200

    Add entities

---
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/utils/entity/Article.kt
@@ -0,0 +1,10 @@
+package com.telesoftas.lithuaniaindependencynews.utils.entity
+
+
+class Article(
+        val title: String,
+        val description: String,
+        val url: String,
+        val urlToImage: String?,
+        val publishedAt: String
+)
---
+++ app/src/main/java/com/telesoftas/lithuaniaindependencynews/utils/entity/ArticleList.kt
@@ -0,0 +1,8 @@
+package com.telesoftas.lithuaniaindependencynews.utils.entity
+
+
+class ArticleList(
+        val status: String,
+        val totalResults: Int,
+        val articles: List<Article>
+)
