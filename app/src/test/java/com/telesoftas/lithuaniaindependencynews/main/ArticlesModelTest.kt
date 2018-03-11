3f52e6d96aae2ca6cd1d11b99258b62d6557a2f6
aleknandrius <aleknandrius@gmail.com>
2018-03-11 13:43:36 +0200

    Update ArtictesModelTest

--- app/src/test/java/com/telesoftas/lithuaniaindependencynews/main/ArticlesModelTest.kt
+++ app/src/test/java/com/telesoftas/lithuaniaindependencynews/main/ArticlesModelTest.kt
@@ -1,47 +1,91 @@
 package com.telesoftas.lithuaniaindependencynews.main
 
 import com.nhaarman.mockito_kotlin.given
+import com.telesoftas.lithuaniaindependencynews.BuildConfig
 import com.telesoftas.lithuaniaindependencynews.utils.entity.Article
 import com.telesoftas.lithuaniaindependencynews.utils.entity.ArticleList
 import io.reactivex.Single
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 
-private val service = Mockito.mock(ArticlesService::class.java)
-private val articles = ArticleList(ArrayList<Article>())
-private val response = Single.just(articles)
+private lateinit var service: ArticlesService
+private val article = Article("", "", "", "", "", "")
+
+private val articlesEmpty = ArticleList(ArrayList())
+private lateinit var articlesFull: ArticleList
+private val responseNotFull = Single.just(articlesEmpty)
+private lateinit var responseFull: Single<ArticleList>
 private lateinit var model: ArticlesModel
 
 class ArticlesModelTest {
     @Before
     fun setUp() {
+        val list = ArrayList<Article>()
+        for (i in 0..19) {
+            list.add(article)
+        }
+        articlesFull = ArticleList(list)
+        responseFull = Single.just(articlesFull)
+        service = Mockito.mock(ArticlesService::class.java)
         model = ArticlesModel(service)
+
+    }
+
+    @Test
+    fun getFirstPage_hasMoreItemsFalse() {
         given(service.articles(
-                Mockito.anyString(),
-                Mockito.anyInt(),
-                Mockito.anyInt(),
-                Mockito.anyString()
-        )).willReturn(response)
+                ArticlesModel.SEARCH_QUERY,
+                1,
+                ArticlesModel.PAGE_SIZE,
+                BuildConfig.API_KEY
+        )).willReturn(responseNotFull)
+        val response = model.getFirstPage().toFuture().get()
+        val responseExpected = responseNotFull.toFuture().get().articles
+        Assert.assertEquals(response, responseExpected)
+        Assert.assertTrue(!model.hasMoreArticles())
     }
 
     @Test
-    fun getFirstPage() {
-
+    fun getFirstPage_hasMoreItemsTrue() {
+        given(service.articles(
+                ArticlesModel.SEARCH_QUERY,
+                1,
+                ArticlesModel.PAGE_SIZE,
+                BuildConfig.API_KEY
+        )).willReturn(responseFull)
+        val response = model.getFirstPage().toFuture().get()
+        val responseExpected = responseFull.toFuture().get().articles
+        Assert.assertEquals(response, responseExpected)
+        Assert.assertTrue(model.hasMoreArticles() == true)
     }
 
     @Test
-    fun getNextPage() {
+    fun getNextPage_hasMoreItemsTrue() {
+        given(service.articles(
+                ArticlesModel.SEARCH_QUERY,
+                1,
+                ArticlesModel.PAGE_SIZE,
+                BuildConfig.API_KEY
+        )).willReturn(responseFull)
+        val response = model.getNextPage().toFuture().get()
+        val responseExpected = responseFull.toFuture().get().articles
+        Assert.assertEquals(response, responseExpected)
+        Assert.assertTrue(model.hasMoreArticles())
     }
 
     @Test
-    fun hasMoreArticles_returnTrue() {
-
+    fun getNextPage_hasMoreItemsFalse() {
+        given(service.articles(
+                ArticlesModel.SEARCH_QUERY,
+                1,
+                ArticlesModel.PAGE_SIZE,
+                BuildConfig.API_KEY
+        )).willReturn(responseNotFull)
+        val response = model.getNextPage().toFuture().get()
+        val responseExpected = responseNotFull.toFuture().get().articles
+        Assert.assertEquals(response, responseExpected)
+        Assert.assertTrue(!model.hasMoreArticles())
     }
-
-    @Test
-    fun hasMoreArticles_returnFalse() {
-
-    }
-
 }
